4e8caa58dc949b46bb17a0a6b841846c3cf5604c
Christopher Birmingham <chris.birmingham@hotmail.co.uk>
2015-10-10 16:12:34 +0100

    changed index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 
 app.get('/', function (req, res) {
@@ -7,6 +8,27 @@ app.get('/', function (req, res) {
 
 app.get('/team', function(req, res){
     res.send("drjhgriufhbeiufhefiuhzbefugfv\niuefheiufheuifhefu\newuifheiufhegui");
+});
+
+app.get('Get.aspx', function(req, res){
+    req.param("username");
+    req.param("age");
+    res.send("Hello" + username)
+});
+
+app.get('/song', function(req, res){
+    var song = req.param("song");
+    console.log(song);
+    fs.readFile(song, function(err, data){
+        if(err){
+            console.log(err);
+            res.send("Error");
+        } else {
+            var response = "";
+            response = "<audio controls><source src=\"data:audio/mp3;base64," + new Buffer(data, 'base64') + "\></audio>"
+            res.send(response);
+        } 
+    });
 });
 
     var server = app.listen(3000, function () {
